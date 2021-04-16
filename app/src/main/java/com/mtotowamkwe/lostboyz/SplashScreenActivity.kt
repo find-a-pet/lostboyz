a42198cc12088d22b4f733011eb89fa55da9a66b
Michael Mkwelele <15795163+mtotoWamkwe@users.noreply.github.com>
2021-04-16 16:44:25 -0700

    Finish splash screen.

--- app/src/main/java/com/mtotowamkwe/lostboyz/SplashScreenActivity.kt
+++ app/src/main/java/com/mtotowamkwe/lostboyz/SplashScreenActivity.kt
@@ -1,11 +1,36 @@
 package com.mtotowamkwe.lostboyz
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
+private const val SPLASH_SCREEN_TIME_OUT = 4000L
 
 class SplashScreenActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (currentFragment == null) {
+            val fragment = SplashScreenFragment.newInstance()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
+
+        val worker = Executors.newSingleThreadScheduledExecutor()
+
+        val runnable = Runnable {
+            val intent: Intent = Intent(this, PetsActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        worker.schedule(runnable, SPLASH_SCREEN_TIME_OUT, TimeUnit.MILLISECONDS)
     }
 }
--- app/src/main/java/com/mtotowamkwe/lostboyz/SplashScreenFragment.kt
+++ app/src/main/java/com/mtotowamkwe/lostboyz/SplashScreenFragment.kt
@@ -1,4 +1,23 @@
 package com.mtotowamkwe.lostboyz
 
-class SplashScreenFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SplashScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_splash_screen, container, false)
+        return view
+    }
+
+    companion object {
+        fun newInstance() = SplashScreenFragment()
+    }
 }
